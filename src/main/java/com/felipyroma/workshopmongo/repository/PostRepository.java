80cec402465ac06d11add6f8442c5d8b2bed4b11
felipyroma <felipy.roma@sptech.school>
2023-05-03 14:37:17 -0300

    Consulta simples com @query

--- src/main/java/com/felipyroma/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/felipyroma/workshopmongo/repository/PostRepository.java
@@ -4,12 +4,18 @@ package com.felipyroma.workshopmongo.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.felipyroma.workshopmongo.domain.Post;
 
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
+	
+
+//	?0 = primeiro parâmetro que veio do metodo
+	@Query("{ 'title': { $regex: ?0, $options: 'i' } }")
+	List<Post> searchTitle(String text);
 
 	List<Post> findByTitleContainingIgnoreCase(String text);
 }
